0189c918f7d73d64e557b07a5119c198c54840bc
maples <14992019+wang-fengxux@user.noreply.gitee.com>
2024-12-02 14:00:07 +0800

    1

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     version='0.0.0',
     packages=find_packages(),
     license='MIT License',
-    long_description=open('README.md').read(),
+    # long_description=open('README.md').read(),
     entry_points={
         # <命令名> = <模块路径>:<函数名>
         'console_scripts': [
             'detr = ACT_plus_plus.detr.setup:setup',
             'robomimic = ACT_plus_plus.robomimic.setup:setup',
-            'mobile-aloha = ACT_plus_plus.mobile-aloha.setup:setup',
+            'mobile_aloha = ACT_plus_plus.mobile_aloha.setup:setup',
         ]
     }
 )
